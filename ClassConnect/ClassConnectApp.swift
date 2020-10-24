97f757ae14d98bcd08d6b33faded6e9163880842
MGCF1 <MGCF007@gmail.com>
2020-10-24 14:41:19 -0400

    Initial Commit

---
+++ ClassConnect/ClassConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassConnectApp.swift
+//  ClassConnect
+//
+//  Created by Milo Cassarino on 10/24/20.
+//
+
+import SwiftUI
+
+@main
+struct ClassConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassConnect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClassConnect
+//
+//  Created by Milo Cassarino on 10/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
